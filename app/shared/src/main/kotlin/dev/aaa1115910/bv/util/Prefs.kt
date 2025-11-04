dc5062cac7f0f3d201190d07cb9e97de4dbe9aec
aaa1115910 <aaa1115910@gmail.com>
2025-11-04 22:12:28 +0800

    新增播放模式选项
    单集播放/顺序播放/单集循环/列表循环
    
    resolves #229

--- app/shared/src/main/kotlin/dev/aaa1115910/bv/util/Prefs.kt
+++ app/shared/src/main/kotlin/dev/aaa1115910/bv/util/Prefs.kt
@@ -20,6 +20,7 @@ import dev.aaa1115910.bv.entity.PlayerType
 import dev.aaa1115910.bv.entity.ThemeType
 import dev.aaa1115910.bv.player.entity.Audio
 import dev.aaa1115910.bv.player.entity.DanmakuType
+import dev.aaa1115910.bv.player.entity.PlayMode
 import dev.aaa1115910.bv.player.entity.Resolution
 import dev.aaa1115910.bv.player.entity.VideoCodec
 import io.github.oshai.kotlinlogging.KotlinLogging
@@ -304,9 +305,16 @@ object Prefs {
             ThemeType.entries[dsm.getPreferenceFlow(PrefKeys.prefThemeTypeRequest).first()]
         }
         set(value) = runBlocking { dsm.editPreference(PrefKeys.prefThemeTypeKey, value.ordinal) }
+
     val themeTypeFlow: Flow<ThemeType>
         get() = dsm.getPreferenceFlow(PrefKeys.prefThemeTypeRequest)
             .transform { ordinal -> emit(ThemeType.entries[ordinal]) }
+
+    var defaultPlayMode: PlayMode
+        get() = runBlocking {
+            PlayMode.entries[dsm.getPreferenceFlow(PrefKeys.prefPlayModeRequest).first()]
+        }
+        set(value) = runBlocking { dsm.editPreference(PrefKeys.prefPlayModeKey, value.ordinal) }
 }
 
 object PrefKeys {
@@ -351,6 +359,7 @@ object PrefKeys {
     val prefDefaultDanmakuMask = booleanPreferencesKey("prefer_enable_webmark")
     val prefEnableFfmpegAudioRenderer = booleanPreferencesKey("enable_ffmpeg_audio_renderer")
     val prefThemeTypeKey = intPreferencesKey("theme_type")
+    val prefPlayModeKey = intPreferencesKey("play_mode")
 
     val prefIsLoginRequest = PreferenceRequest(prefIsLoginKey, false)
     val prefUidRequest = PreferenceRequest(prefUidKey, 0)
@@ -407,4 +416,5 @@ object PrefKeys {
     val prefDefaultDanmakuMaskRequest = PreferenceRequest(prefDefaultDanmakuMask, false)
     val prefEnableFfmpegEndererRequest = PreferenceRequest(prefEnableFfmpegAudioRenderer, false)
     val prefThemeTypeRequest = PreferenceRequest(prefThemeTypeKey, ThemeType.Auto.ordinal)
+    val prefPlayModeRequest = PreferenceRequest(prefPlayModeKey, PlayMode.Sequential.ordinal)
 }
