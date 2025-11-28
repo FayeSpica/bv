d0196af12f15e793f5410ec012f811e277dc65fc
fantasytyx <125529767+fantasytyx@users.noreply.github.com>
2025-11-28 23:07:40 +0800

    修复 request was banned 请求错误 (#276)
    * 修复 request was banned 请求错误
    
    * 修复 pgc 接口请求错误
    
    * 根据设备信息生成 UserAgent
    
    ---------
    
    Co-authored-by: aaa1115910 <aaa1115910@gmail.com>

--- bili-api/src/main/kotlin/dev/aaa1115910/biliapi/http/BiliLiveHttpApi.kt
+++ bili-api/src/main/kotlin/dev/aaa1115910/biliapi/http/BiliLiveHttpApi.kt
@@ -6,11 +6,11 @@ import dev.aaa1115910.biliapi.http.entity.live.HistoryDanmaku
 import dev.aaa1115910.biliapi.http.entity.live.LiveFollowingData
 import dev.aaa1115910.biliapi.http.entity.live.LiveStreamUrlData
 import dev.aaa1115910.biliapi.http.entity.live.RoomPlayInfoData
+import dev.aaa1115910.biliapi.http.plugins.BiliUserAgent
 import io.github.oshai.kotlinlogging.KotlinLogging
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.BrowserUserAgent
 import io.ktor.client.plugins.compression.ContentEncoding
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
@@ -32,7 +32,7 @@ object BiliLiveHttpApi {
 
     private fun createClient() {
         client = HttpClient(OkHttp) {
-            BrowserUserAgent()
+            BiliUserAgent()
             install(ContentNegotiation) {
                 json(Json {
                     coerceInputValues = true
